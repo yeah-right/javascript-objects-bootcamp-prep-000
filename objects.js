3e798fd4ea0c17632fb60d4373b676da30d9749b
Zack A Claar <claar.zack@gmail.com>
2019-02-16 16:26:37 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,4 +4,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign(playlist, { [artistName]: songTitle })
 }
 
-function removeFromPlaylist(playlist,)
+function removeFromPlaylist(playlist, artistName) {
+  return delete playlist.artistName;
+}
