c4bf2f809ce0a52d2f546e4a9197a579218899b9
Zack A Claar <claar.zack@gmail.com>
2019-02-16 16:34:37 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,14 +1,14 @@
 var playlist = new Object({ 'Slowdive': 'Alison'});
-console.log(playlist);
+// console.log(playlist);
 
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign(playlist, { [artistName]: songTitle })
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  console.log(playlist);
+  // console.log(playlist);
   delete playlist.artistName;
-  console.log(playlist);
+  // console.log(playlist);
 }
 
 removeFromPlaylist(playlist, 'Alison');
