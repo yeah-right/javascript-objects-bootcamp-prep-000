718033a2b2ba379d2fd11cf17f2623d0776f2806
Zack A Claar <claar.zack@gmail.com>
2019-02-16 16:13:28 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = new Object({ artist: "song"});
 
-function updatePlaylist(playlist, name, title) {
-  return Object.assign({}, playlist, {[name]: title})
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign({}, playlist, {[artistName]: songTitle})
 }
