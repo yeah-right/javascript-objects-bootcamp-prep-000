afd2055e1cfc083e470af0aa40eb32ddb2bb0e51
Zack A Claar <claar.zack@gmail.com>
2019-02-16 16:30:50 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,6 +5,5 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName;
-  return playlist;
+  return delete playlist.artistName;
 }
