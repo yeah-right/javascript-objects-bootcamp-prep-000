ba080f5eb83817712009cdf09fc6840e9cdb8aac
Zack A Claar <claar.zack@gmail.com>
2019-02-16 16:24:13 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = new Object({ 'Slowdive': 'Alison'});
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, playlist, {[artistName]: songTitle});
+  return Object.assign({}, playlist, { [artistName]: songTitle })
 }
